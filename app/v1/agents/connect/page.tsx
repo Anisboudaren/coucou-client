5dbe009a23f0dbd27df74a604f5e4d8f23bad41f
Anis <anisboudaren2@gmail.com>
2025-06-09 01:05:56 +0100

    script file only for prod

--- app/v1/agents/connect/page.tsx
+++ app/v1/agents/connect/page.tsx
@@ -48,7 +48,8 @@ export default function Page() {
   }, []);
 
   // const scriptFile = process.env.NODE_ENV === 'production' ? 'bubble-prod.js' : 'bubble.js';
-  const scriptFile = process.env.VERCEL_ENV === 'production' ? 'bubble-prod.js' : 'bubble.js';
+  // for testing sake
+  const scriptFile = 'bubble-prod.js';
 
   const scriptSnippet = selectedAgentId
     ? `<script 
